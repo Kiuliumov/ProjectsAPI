6afba8de0b541d6f0c9eb36a16190ca93a7b322f
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-02-07 08:03:00 +0200

    Init

--- ProjectAPI/urls.py
+++ ProjectAPI/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', 'projects.urls'),
-    path('tasks/', 'tasks.urls'),
-    path('auth/', 'auth.urls')
+    path('projects/', include('projects.urls')),
+    path('tasks/', include('tasks.urls')),
+    path('auth/', include('auth.urls'))
 ]
